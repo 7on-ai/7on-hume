583e90e6862720786b92a6eac1b990a7183f09aa
7on-ai <team7on.ai@gmail.com>
2025-08-06 23:35:49 +0700

    Update Sevenon.tsx

--- components/logos/Sevenon.tsx
+++ components/logos/Sevenon.tsx
@@ -1,50 +1,17 @@
-import * as React from "react"
-import { SVGProps } from "react"
+// components/logos/Logo.tsx
+import React from "react";
 
-const Sevenon = (props: SVGProps<SVGSVGElement>) => (
+const Sevenon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
-    width={180}
-    height={177}
+    width="100"
+    height="100"
+    viewBox="0 0 100 100"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <path
-      d="M121.779 92.234c0 4.274-.842 8.506-2.477 12.455a32.557 32.557 0 0 1-17.613 17.613 32.556 32.556 0 0 1-24.909 0 32.554 32.554 0 0 1-17.613-17.613 32.548 32.548 0 0 1 7.055-35.467 32.545 32.545 0 0 1 55.557 23.013Z"
-      stroke="#000"
-      strokeWidth={5.424}
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path
-      d="M119.875 92.5c0 16.774-13.601 30.375-30.375 30.375S59.125 109.274 59.125 92.5 72.726 62.125 89.5 62.125s30.375 13.601 30.375 30.375Z"
-      fill="#0C0C0F"
-      stroke="#0C0C0F"
-      strokeWidth={0.409}
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M93.495 68.53a3.995 3.995 0 0 0-7.99 0v20.283a3.995 3.995 0 0 0 7.99 0V68.53Zm-16.287 8.226a3.994 3.994 0 0 0-4.914-6.3 27.964 27.964 0 1 0 34.416 0 3.996 3.996 0 0 0-4.918 6.3 19.968 19.968 0 0 1 6.858 10.067 19.978 19.978 0 0 1-19.15 25.652A19.979 19.979 0 0 1 70.613 99a19.974 19.974 0 0 1 6.595-22.244Z"
-      fill="#fff"
-    />
-    <path
-      d="M99.625 92.5c0 5.591-4.534 10.125-10.125 10.125S79.375 98.091 79.375 92.5 83.909 82.375 89.5 82.375 99.625 86.909 99.625 92.5Z"
-      fill="#fff"
-      stroke="#fff"
-      strokeWidth={0.338}
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M89.428 66.09c1.035 0 2.027.287 2.758.797.732.51 1.143 1.201 1.143 1.922v43.501c0 .721-.411 1.412-1.143 1.922-.731.51-1.723.796-2.758.796-1.034 0-2.027-.286-2.758-.796-.732-.51-1.143-1.201-1.143-1.922v-43.5c0-.722.411-1.413 1.143-1.923.731-.51 1.724-.797 2.758-.797Z"
-      fill="#fff"
-    />
+    <circle cx="50" cy="50" r="50" fill="red" />
   </svg>
-)
+);
 
-export default Sevenon
+export default Sevenon;
