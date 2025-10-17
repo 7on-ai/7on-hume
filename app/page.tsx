206cf6cc0e3a7e53b20878db449c1dc3037fd622
7on-ai <team7on.ai@gmail.com>
2025-10-17 17:27:24 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-import dynamic from "next/dynamic";
 import { getHumeAccessToken } from "@/utils/getHumeAccessToken";
-
-const Chat = dynamic(() => import("@/components/Chat"), {
-  ssr: false,
-});
-
-const MatrixRain = dynamic(() => import("@/components/matrix-rain"), {
-  ssr: false,
-});
+import ClientWrapper from "@/components/ClientWrapper";
 
 export default async function Page() {
   const accessToken = await getHumeAccessToken();
@@ -16,23 +8,5 @@ export default async function Page() {
     throw new Error('Unable to get access token');
   }
 
-  return (
-    <>
-      {/* Matrix Rain Background - Fixed position, lowest z-index */}
-      <MatrixRain />
-      
-      {/* Chat Interface - Relative position, higher z-index */}
-      <div style={{
-        position: 'relative',
-        zIndex: 10,
-        width: '100%',
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        pointerEvents: 'auto'
-      }}>
-        <Chat accessToken={accessToken} />
-      </div>
-    </>
-  );
+  return <ClientWrapper accessToken={accessToken} />;
 }
