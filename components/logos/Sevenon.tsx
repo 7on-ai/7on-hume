88c733ead5e1f2d5fdad289a4491f7a3cb099d20
7on-ai <team7on.ai@gmail.com>
2025-10-17 13:42:05 +0700

    Update Sevenon.tsx

--- components/logos/Sevenon.tsx
+++ components/logos/Sevenon.tsx
@@ -1,71 +1,40 @@
-import * as React from "react"
-import { SVGProps } from "react"
+import React from 'react';
 
-const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
-  <svg
-    width={250}
-    height={250}
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
-    <g filter="url(#a)">
-      <path
-        d="M208 124.816a83.003 83.003 0 0 1-51.237 76.682 83.003 83.003 0 0 1-108.445-44.919A83.001 83.001 0 1 1 208 124.816Z"
-        fill="#F8F8F8"
-        stroke="#F44336"
-        strokeWidth={3}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </g>
-    <path
-      d="m168.691 87.42 6.48 11.268-71.009-.124-3.472-.001 1.728 3.006 20.174 35.061-5.916 10.219L82.62 87.625l.166-.289 85.905.083Zm8.283 15.279-43.837 75.636-13.758-.024 36.295-62.616 1.744-3.006-3.472-.001-40.541-.066-5.781-10.045 69.35.122Zm-26.513 13.991-35.255 60.824-42.17-73.338 7.266-12.547 34.626 60.224 1.733 3.012 1.74-3.01 20.381-35.18 11.679.015Z"
-      fill="#FF6B5B"
-    />
-    <path
-      d="m176.974 102.699-43.837 75.636-13.758-.024 36.295-62.616 1.744-3.006-3.472-.001-40.541-.066-5.781-10.045 69.35.122Z"
-      fill="#F44336"
-    />
-    <defs>
-      <filter
-        id="a"
-        x={0.5}
-        y={0.316}
-        width={249}
-        height={249}
-        filterUnits="userSpaceOnUse"
-        colorInterpolationFilters="sRGB"
-      >
-        <feFlood floodOpacity={0} result="BackgroundImageFix" />
-        <feColorMatrix
-          in="SourceAlpha"
-          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-          result="hardAlpha"
-        />
-        <feOffset />
-        <feGaussianBlur stdDeviation={20} />
-        <feComposite in2="hardAlpha" operator="out" />
-        <feColorMatrix values="0 0 0 0 1 0 0 0 0 0.415686 0 0 0 0 0.301961 0 0 0 0.2 0" />
-        <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_0_1" />
-        <feBlend
-          in="SourceGraphic"
-          in2="effect1_dropShadow_0_1"
-          result="shape"
-        />
-        <feColorMatrix
-          in="SourceAlpha"
-          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-          result="hardAlpha"
-        />
-        <feOffset />
-        <feGaussianBlur stdDeviation={4} />
-        <feComposite in2="hardAlpha" operator="arithmetic" k2={-1} k3={1} />
-        <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.08 0" />
-        <feBlend in2="shape" result="effect2_innerShadow_0_1" />
-      </filter>
-    </defs>
+import React from 'react';
+  
+  // Define props type for better TypeScript integration
+  interface SevenonSvgProps extends React.SVGProps<SVGSVGElement> {
+    // Add any custom props specific to this icon if needed
+    // size?: number | string;
+  }
+  
+  const SevenonSvg: React.FC<SevenonSvgProps> = (props) => (
+    <svg {...props} width="250" height="250" viewBox="0 0 250 250" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <g filter="url(#filter0_di_0_1)">
+  <path d="M208 124.816C208 135.716 205.853 146.509 201.682 156.579C197.511 166.649 191.397 175.799 183.69 183.506C175.983 191.213 166.833 197.327 156.763 201.498C146.693 205.669 135.9 207.816 125 207.816C114.1 207.816 103.307 205.669 93.2373 201.498C83.1673 197.327 74.0174 191.213 66.3101 183.506C58.6029 175.799 52.4891 166.649 48.318 156.579C44.1469 146.509 42 135.716 42 124.816C42 102.803 50.7446 81.6917 66.3101 66.1262C81.8757 50.5607 102.987 41.8161 125 41.8161C147.013 41.8161 168.124 50.5607 183.69 66.1262C199.255 81.6917 208 102.803 208 124.816Z" fill="#F8F8F8" stroke="#F44336" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+  </g>
+  <path d="M168.691 87.4193L175.171 98.688L104.162 98.5635L100.69 98.5626L102.418 101.569L122.592 136.63L116.676 146.849L82.62 87.6252L82.7861 87.3363L168.691 87.4193ZM176.974 102.699L133.137 178.335L119.379 178.311L155.674 115.695L157.418 112.689L153.946 112.688L113.405 112.622L107.624 102.577L176.974 102.699ZM150.461 116.69L115.206 177.514L73.0354 104.176L80.3017 91.6288L114.928 151.853L116.661 154.865L118.401 151.855L138.782 116.675L150.461 116.69Z" fill="#FF6B5B"/>
+  <path d="M176.974 102.699L133.137 178.335L119.379 178.311L155.674 115.695L157.418 112.689L153.946 112.688L113.405 112.622L107.624 102.577L176.974 102.699Z" fill="#F44336"/>
+  <defs>
+  <filter id="filter0_di_0_1" x="0.5" y="0.316082" width="249" height="249" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+  <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+  <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+  <feOffset/>
+  <feGaussianBlur stdDeviation="20"/>
+  <feComposite in2="hardAlpha" operator="out"/>
+  <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 0.415686 0 0 0 0 0.301961 0 0 0 0.2 0"/>
+  <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_0_1"/>
+  <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_0_1" result="shape"/>
+  <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+  <feOffset/>
+  <feGaussianBlur stdDeviation="4"/>
+  <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+  <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.08 0"/>
+  <feBlend mode="normal" in2="shape" result="effect2_innerShadow_0_1"/>
+  </filter>
+  </defs>
   </svg>
-)
-
-export default SvgComponent
+  
+  );
+  
+  export default SevenonSvg;
